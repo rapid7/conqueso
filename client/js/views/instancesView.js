1e6fbd30c386d5ef6051a0e59b4229b826112691
Steve Willard <stevewillard@gmail.com>
2014-02-03 18:03:19 -0500

    Adding instances view

---
+++ client/js/views/instancesView.js
@@ -0,0 +1,43 @@
+/**
+* COPYRIGHT (C) 2014, Rapid7 LLC, Boston, MA, USA.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+define(["jquery", "backbone", "underscore", "../broadcast", "../models/role", "../collections/properties",
+        "hbars!templates/instances.template"],
+function($, Backbone, _, Broadcast, RoleModel, PropertiesCollection,
+         instancesTemplate) {
+    
+    return Backbone.View.extend({
+        el : "#main-content",
+
+        initialize: function() {
+        },
+
+        events : {
+        },
+
+        fetchCallback: function(model) {
+            this.properties = new PropertiesCollection(model.properties, {name : model.get("name")});
+            this.$el.html(instancesTemplate(model.toJSON()));
+            Broadcast.trigger("role:change", {name : model.get("name"), silent : true});
+        },
+
+        render: function(roleName) {
+            this.roleName = roleName;
+            this.role = new RoleModel({id : roleName});
+            this.role.getProperties(_.bind(this.fetchCallback, this));
+        }
+    });
+});
