ebb800ed01a62873e1309017c4cb0dfd6c57f82f
Steve Willard <stevewillard@gmail.com>
2014-02-04 13:06:26 -0500

    Adding unit tests for filtering

--- test/utils.js
+++ test/utils.js
@@ -6,30 +6,44 @@ describe("utils", function() {
         it("should return empty string for null properties", function() {
             assert.equal("", utils.propertiesToTextPlain(null));
         });
-    });
 
-    describe("#propertiesToTextPlain", function(){
         it("should return empty string for empty properties", function() {
             assert.equal("", utils.propertiesToTextPlain([]));
         });
-    });
 
-    describe("#propertiesToTextPlain", function(){
         it("should return plain text of single property", function() {
-        	var result = utils.propertiesToTextPlain([{
-        		name  : "foo",
-        		value : "bar"
-        	}]);
+            var result = utils.propertiesToTextPlain([{
+                name  : "foo",
+                value : "bar"
+            }]);
             assert.equal("foo=bar\n", result);
+        });
+
+        it("should return plain text of multiple properties", function() {
+            var result = utils.propertiesToTextPlain([
+            { name : "foo", value : "bar" },
+            { name : "a", value : "b"}]);
+            assert.equal("foo=bar\na=b\n", result);
         });
     });
 
-    describe("#propertiesToTextPlain", function(){
-        it("should return plain text of multiple properties", function() {
-        	var result = utils.propertiesToTextPlain([
-        	{ name : "foo", value : "bar" },
-        	{ name : "a", value : "b"}]);
-            assert.equal("foo=bar\na=b\n", result);
+    describe("#filterProperties", function(){
+        it("should filter down to only one property", function() {
+            var properites = [{ "name" : "foo", "value" : "val1" },
+                              { "name" : "bar", "value" : "val2" }],
+                result = utils.filterProperties(properites, "foo");
+            assert.equal(result.length, 1);
+        });
+
+        it("nonexistent name should filter to nothing", function() {
+            var properites = [{ "name" : "foo", "value" : "val1" }],
+                result = utils.filterProperties(properites, "bar");
+            assert.equal(result.length, 0);
+        });
+
+        it("should return empty list", function() {
+            var result = utils.filterProperties([], "bar");
+            assert.equal(result.length, 0);
         });
     });
 });
