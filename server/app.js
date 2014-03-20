108dfba9a1e4ddd1f43c648646d1d95de28000cf
Steve Willard <stevewillard@gmail.com>
2014-03-20 03:13:55 -0400

    Add basic support for clustering

--- server/app.js
+++ server/app.js
@@ -14,28 +14,59 @@
 * limitations under the License.
 */
 
-if (process.env.NODE_ENV === "production" && process.setuid && process.setgid) {
-    process.setgid("conqueso");
-    process.setuid("conqueso");
-}
-
-var logger = require("./logger");
-logger.info("Starting Conqueso server");
-
 var express = require("express"),
     app = express(),
-    PersistenceService = require("./db/persistenceService"),
-    port = require("./config/settings").getHttpPort(),
-    persistenceService = new PersistenceService(function() {
-        // Load server default properties and roles if defaults.json file specified
-        require("./propertyLoader")(persistenceService);
+    cluster = require("cluster"),
+    config = require("./config/settings"),
+    logger = require("./logger"),
+    port = config.getHttpPort();
 
-        app.listen(port, function() {
-            logger.info("Listening on port %d", port);
-        });
+if (cluster.isMaster) {
+    logger.info("Starting Conqueso server");
+}
+
+if (!config.isClusteringEnabled()) {
+    process.env.isMaster = true;
+}
+
+if (config.isClusteringEnabled() && cluster.isMaster) {
+    var cpus = require("os").cpus().length,
+        workers = config.getClusterCount() || cpus;
+
+    logger.info("Clustering enabled.", {CPUs:cpus, workers:workers});
+
+    for (var i = 0; i < workers; i++) {
+        var envData = (i === 0) ? {isMaster : true} : {};
+        cluster.fork(envData);
+    }
+
+    cluster.on("online", function(worker) {
+        logger.info( "Worker online", {pid: worker.process.pid});
     });
 
-require("./routes/web")(express, app);
-require("./routes/api")(express, app, persistenceService);
-require("./serviceTracker")(persistenceService);
+    // Re-fork when worker dies
+    cluster.on("exit", function () {
+        cluster.fork();
+    });
+} else {
+    var PersistenceService = require("./db/persistenceService"),
+        persistenceService = new PersistenceService(function() {
+            // Load server default properties and roles if defaults.json file specified
+            require("./propertyLoader")(persistenceService);
 
+            app.listen(port, function() {
+                if (process.env.isMaster) {
+                    logger.info("Listening on port %d", port);
+                }
+                
+                if (process.env.NODE_ENV === "production" && process.setuid && process.setgid) {
+                    process.setgid("conqueso");
+                    process.setuid("conqueso");
+                }
+            });
+        });
+
+    require("./routes/web")(express, app);
+    require("./routes/api")(express, app, persistenceService);
+    require("./serviceTracker")(persistenceService);
+}
